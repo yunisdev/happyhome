ef7c12a8c58aed2641506ed827c8ab38e97a759f
yunidev <yunidev.04@gmail.com>
2020-05-04 21:49:23 +0300

    http

--- src/index.js
+++ src/index.js
@@ -6,12 +6,27 @@ const cookieParser = require('cookie-parser')
 const partialsPath = path.join(__dirname, '../views/partials')
 const publicDirectoryPath = path.join(__dirname, '../public')
 const port = process.env.PORT
+const fs = require('fs')
+
+const http = require('http');
+const https = require('https');
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/happyhome.tc/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/happyhome.tc/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/happyhome.tc/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 require('./db/db')
 app.use(express.urlencoded())
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath, { dotfiles: 'allow' } ))
 app.set('view engine', 'hbs')
 hbs.registerPartials(partialsPath)
 
@@ -27,6 +42,14 @@ app.get('*', (req, res) => {
     res.render('404',{subscribed: req.cookies.subscribed})
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
