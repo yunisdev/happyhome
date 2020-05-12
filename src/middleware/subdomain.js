68a4a29adc8f97b4b3dc0b477835b00210b0814e
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-05-12 12:11:33 +0400

    subdomain

--- src/middleware/subdomain.js
+++ src/middleware/subdomain.js
@@ -3,8 +3,10 @@ const subdomainRedirect = (options = {}) => {
         var redirect = options[req.headers.host.split('.')[0]]
         if (redirect) {
             if (redirect.split('/')[0] === 'f') {
-                return res.redirect('http://' + req.headers.host.split('.')[1]+ req.headers.host.split('.')[2] || '' + redirect.slice(1))
-            }else if(redirect.split('/')[0] === 'm'){
+                var extension = req.headers.host.split('.')[2] ? req.headers.host.split('.')[2] : ''
+                return res.redirect('http://' + req.headers.host.split('.')[1] + extension + redirect.slice(1))
+            } else if (redirect.split('/')[0] === 'm') {
+                var extension = req.headers.host.split('.')[2] ? req.headers.host.split('.')[2] : ''
                 return res.send(`
                 <head>
                     <style>
@@ -16,7 +18,7 @@ const subdomainRedirect = (options = {}) => {
                     </style>
                 </head>
                 <body>
-                    <iframe style="width:100%;height:100vh" src="${'http://' + req.headers.host.split('.')[1] + redirect.slice(1)}" ></iframe>
+                    <iframe style="width:100%;height:100vh" src="${'http://' + req.headers.host.split('.')[1] + extension + redirect.slice(1)}" ></iframe>
                 </body>
                 `)
             }
