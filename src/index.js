d2a47284589fe32132866caae886cfe29f8daa9f
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-07-29 16:21:12 +0400

    port

--- src/index.js
+++ src/index.js
@@ -36,29 +36,6 @@ app.get('*', (req, res) => {
 	res.render('404', { subscribed: req.cookies.subscribed })
 })
 
-if (process.env.NODE_ENV == 'development') {
-	app.listen(port, () => {
-		console.log('Development server running on ' + port)
-	})
-} else if (process.env.NODE_ENV == 'production') {
-	// Certificate
-	const privateKey = fs.readFileSync('/etc/letsencrypt/live/happyhome.tc/privkey.pem', 'utf8');
-	const certificate = fs.readFileSync('/etc/letsencrypt/live/happyhome.tc/cert.pem', 'utf8');
-	const ca = fs.readFileSync('/etc/letsencrypt/live/happyhome.tc/chain.pem', 'utf8');
-
-	const credentials = {
-		key: privateKey,
-		cert: certificate,
-		ca: ca
-	};
-
-	const httpServer = http.createServer(app);
-	const httpsServer = https.createServer(credentials, app);
-	httpServer.listen(80, () => {
-		console.log('HTTP Server running on port 80');
-	});
-
-	httpsServer.listen(443, () => {
-		console.log('HTTPS Server running on port 443');
-	});
-}
+app.listen(port, () => {
+	console.log('Development server running on ' + port)
+})
